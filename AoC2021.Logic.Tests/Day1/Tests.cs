e9e91ff86c788b1cc3305e61e09bd83d9c7ab300
eisbaer66 <janweidner87@gmail.com>
2021-12-01 06:53:44 +0100

    Add project files.

---
+++ AoC2021.Logic.Tests/Day1/Tests.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using NUnit.Framework;
+
+namespace AoC2021.Logic.Tests.Day1
+{
+    public class Tests
+    {
+        [Test]
+        [TestCase("Day1\\Example.txt", 7)]
+        [TestCase("Day1\\Input.txt",   1564)]
+        public void Part1(string filename, int expectedSlope)
+        {
+            var input      = File.ReadAllText(filename);
+            var sonarSweep = new SonarSweep();
+            sonarSweep.Load(input);
+
+            Assert.AreEqual(expectedSlope, sonarSweep.GetSlope(), "incorrect slope");
+        }
+
+        [Test]
+        [TestCase("Day1\\Example.txt", 5)]
+        [TestCase("Day1\\Input.txt",   1611)]
+        public void Part2(string filename, int expectedSlope)
+        {
+            var input      = File.ReadAllText(filename);
+            var sonarSweep = new SonarSweep();
+            sonarSweep.Load(input);
+
+            Assert.AreEqual(expectedSlope, sonarSweep.GetSlidingWindowSlope(3), "incorrect sliding window slope");
+        }
+    }
+}
---
+++ AoC2021.Logic/SonarSweep.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2021.Logic
+{
+    public class SonarSweep
+    {
+        private int[] _readings;
+
+        public void Load(string input)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input));
+
+            _readings = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+        }
+
+        public int GetSlope()
+        {
+            return GetSlope(_readings);
+        }
+
+        public int GetSlidingWindowSlope(int windowSize)
+        {
+            var slidingWindows = GetSlidingWindows(_readings, windowSize)
+                .Select(window => window.Sum());
+
+            return GetSlope(slidingWindows);
+        }
+
+        private IEnumerable<IEnumerable<int>> GetSlidingWindows(IReadOnlyList<int> readings, int windowSize)
+        {
+            IList<IList<int>> slidingWindows = new List<IList<int>>();
+            var               windowsCount   = readings.Count - windowSize + 1;
+            for (int i = 0; i < windowsCount; i++)
+            {
+                slidingWindows.Add(new List<int>());
+
+                for (int j = i; j < i + windowSize; j++)
+                {
+                    slidingWindows[i].Add(readings[j]);
+                }
+            }
+
+            return slidingWindows;
+        }
+
+        private int GetSlope(IEnumerable<int> readings)
+        {
+            var slope           = 0;
+            var previousReading = int.MaxValue;
+            foreach (var reading in readings)
+            {
+                if (previousReading < reading)
+                    ++slope;
+                previousReading = reading;
+            }
+
+            return slope;
+        }
+    }
+}
